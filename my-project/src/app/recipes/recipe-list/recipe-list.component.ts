285310ddcf213d822d7a957c2bf40af518f9b750
david <davidleon_06@hotmail.com>
2023-03-01 18:26:35 +0100

    Added styling and structure to project

--- my-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://www.washingtonpost.com/resizer/6mbSPgQOvUWRmmNnirvgvLzutio=/arc-anglerfish-washpost-prod-washpost/public/VA56ZXQQMUI63CHIYWG4HW5O4I.jpg'
+    ),
+    new Recipe(
+      'A test Recipe',
+      'This is a test',
+      'https://www.washingtonpost.com/resizer/6mbSPgQOvUWRmmNnirvgvLzutio=/arc-anglerfish-washpost-prod-washpost/public/VA56ZXQQMUI63CHIYWG4HW5O4I.jpg'
+    ),
+  ];
 }
